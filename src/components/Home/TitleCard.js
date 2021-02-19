cc31e06cb775b56687737c649f8630f22c6acca6
kcshiffl <kcshiffl@gmail.com>
2021-02-19 02:15:06 -0500

    Fixed scaling issues with homepage logo

--- src/components/Home/TitleCard.js
+++ src/components/Home/TitleCard.js
@@ -3,7 +3,7 @@ import logo from '../images/Me_Icon.png';
 const TitleCard = () => {
   return (
     <div className='title-card'>
-      <img className='logo-display' src={logo} absolute width='50%' absolute height='50%'/>
+      <img className='logo-display' src={logo} />
     </div>
   )
 }
